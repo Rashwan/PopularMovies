b53573236e9b2cfb5e4a9195b5f48fabd372cc78
ahmed_rashwan <ahmed.t.rashwan@gmail.com>
2015-09-18 06:51:18 +0200

    Fix Reviews Not Showing.

--- app/src/main/java/com/example/rashwan/popularmovies/MovieDetalisActivity.java
+++ app/src/main/java/com/example/rashwan/popularmovies/MovieDetalisActivity.java
@@ -1,7 +1,7 @@
 package com.example.rashwan.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
